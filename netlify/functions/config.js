bc253657f15e3f7ff2e9fef8f06efc7583dc3166
tnd2x5 <tnd2x5@gmail.com>
2025-09-15 21:04:15 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '7872561320:AAEYeMyxHgN8PA2PahCWyMlZiMywqIcT9Tw',
-    chat_id: '-4971132212',
+    token: '7896416026:AAHJCytsk15GUaBxJi6_JA6B4Te4asALyUU',
+    chat_id: '1819276982',
     noti_token: '',
     noti_chat_id: ''
 };
